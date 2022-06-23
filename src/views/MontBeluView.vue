0fef487869ac26fc690ea85de787ec7c0d3c3ee8
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2022-06-23 18:21:50 -0400

    Update MontBeluView.vue

--- src/views/MontBeluView.vue
+++ src/views/MontBeluView.vue
@@ -22,8 +22,8 @@
 
     <div class="row featurette">
       <div class="col-md-12">
-        <p class="lead">Le Mont Bélu offre des sentiers de type « enduro » depuis 2012. Un sentier d’ascension et 6 sentiers descendant de tous les niveaux sont disponibles. Un seul sentier a été fabriqué mécaniquement, les 5 autres sentiers ont tous été sculptés à la main par des passionnés afin d’offrir des descentes naturelles et une expérience unique. La remontée mécanique en T-Bar est accessible deux jours par semaine pendant la saison estivale. Avec en prime une vue imprenable sur l’arrondissement La Baie et le Fjord du Saguenay. Visitez notre page facebook pour plus de détails!</p>
-        <p><a href="https://www.trailforks.com/region/montbelu-bike-park" target="_blank" class="btn btn-md btn-secondary">Carte des sentiers</a> <a href="https://www.facebook.com/montbelubikepark/" target="_blank" class="btn btn-md btn-secondary">Facebook</a></p>
+        <p class="lead">Le Mont Bélu offre des sentiers de type « enduro » depuis 2012. Un sentier d’ascension et 6 sentiers descendant de tous les niveaux sont disponibles. Un seul sentier a été fabriqué mécaniquement, les 5 autres sentiers ont tous été sculptés à la main par des passionnés afin d’offrir des descentes naturelles et une expérience unique. La remontée mécanique en T-Bar est accessible deux jours par semaine pendant la saison estivale. Avec en prime une vue imprenable sur l’arrondissement La Baie et le Fjord du Saguenay. Visitez notre <a href="https://www.facebook.com/montbelubikepark/" target="_blank">page facebook</a> pour plus de détails!</p>
+        <p><a href="https://www.trailforks.com/region/montbelu-bike-park" target="_blank" class="btn btn-md btn-secondary">Carte des sentiers</a> <a class="btn btn-md btn-secondary" href="https://secure3.xpayrience.com/plateforme_acces_velo_chicoutimi" target="_blank">Billetterie</a></p>
       </div>
     </div>
 

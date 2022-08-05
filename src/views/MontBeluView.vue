81576b86c696a35512155cdab7a966a0239c169d
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2022-08-05 10:55:30 -0400

    Update MontBeluView.vue

--- src/views/MontBeluView.vue
+++ src/views/MontBeluView.vue
@@ -21,17 +21,23 @@
     <!-- START THE FEATURETTES -->
 
     <div class="row featurette">
-      <div class="col-md-12">
+      <div class="col-md-7">
         <p class="lead">Le Mont Bélu offre des sentiers de type « enduro » depuis 2012. Un sentier d’ascension et 6 sentiers descendant de tous les niveaux sont disponibles. Un seul sentier a été fabriqué mécaniquement, les 5 autres sentiers ont tous été sculptés à la main par des passionnés afin d’offrir des descentes naturelles et une expérience unique. La remontée mécanique en T-Bar est accessible deux jours par semaine pendant la saison estivale. Avec en prime une vue imprenable sur l’arrondissement La Baie et le Fjord du Saguenay. Visitez notre <a href="https://www.facebook.com/montbelubikepark/" target="_blank">page facebook</a> pour plus de détails!</p>
         <p>
           <a class="btn btn-md btn-secondary m-2" href="https://www.trailforks.com/region/montbelu-bike-park" target="_blank">Carte des sentiers (Trailfork)</a>
           <a class="btn btn-md btn-secondary m-2" href="https://secure3.xpayrience.com/plateforme_acces_velo_chicoutimi" target="_blank">Billetterie Sentier d'ascention</a></p>
+      </div>
+      <div class="col-md-5">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/jCSda8AfpVc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
     </div>
 
     <hr class="featurette-divider">
 
     <div class="row featurette">
+      <div class="col-md-5">
+        <img src="@/assets/media/images/gallery/belu_3_500.jpg" class="rounded img-fluid" alt="">
+      </div>
       <div class="col-md-7">
         <h2 class="featurette-heading">Remontée mécanique</h2>
           <ul class="lead">
@@ -40,15 +46,12 @@
             <li>Billetterie sur place seulement</li>
           </ul>
       </div>
-      <div class="col-md-5">
-        <img src="@/assets/media/images/gallery/belu_3_500.jpg" class="rounded img-fluid" alt="">
-      </div>
     </div>
 
     <hr class="featurette-divider">
 
     <div class="row featurette">
-      <div class="col-md-7 order-md-2">
+      <div class="col-md-7">
         <h2 class="featurette-heading">Sentier d'ascention</h2>
         <ul class="lead">
           <li>Accès 7 jours sur 7</li>
@@ -56,7 +59,7 @@
         </ul>
         <p><a class="btn btn-md btn-secondary" href="https://secure3.xpayrience.com/plateforme_acces_velo_chicoutimi" target="_blank">Billetterie</a></p>
       </div>
-      <div class="col-md-5 order-md-1">
+      <div class="col-md-5">
         <img src="@/assets/media/images/gallery/belu_4_500.jpg" class="rounded img-fluid" alt="">
       </div>
     </div>
